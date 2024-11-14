cae40ed7dc187751cbb3d5e3637eb2d0cb1b50ac
baopowerpoint <89951510+baopowerpoint@users.noreply.github.com>
2024-11-15 00:17:44 +0700

    add asset, set up light and dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 
-import Hello from "@/components/Hello";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Home = () => {
   console.log("what am i doing here");
   return (
     <div>
-      <h1 className="text-3xl font-black text-white"> Home</h1>
-      <Hello />
+      <h1 className="text-3xl font-black "> Home</h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
